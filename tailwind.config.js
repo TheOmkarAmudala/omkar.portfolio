baa3833519585b1f3a30e7c153dbeeee28a48649
ASUS <22u41a4205@diet.edu.in>
2024-07-08 10:32:13 +0530

    desktop version ready

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: { fontFamily: {
+        'urbanist': ['Urbanist', 'sans-serif'],
+      },},
+    screens: {
+      'mobile': '320px',  // Custom breakpoint for smaller screens
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    }
+  },
+  plugins: [],
+}
