73453af0e8efb66013be7df5471daed6eb18699c
ASUS <22u41a4205@diet.edu.in>
2024-07-10 17:12:33 +0530

    desktop version ready

--- src/components/homemid.js
+++ src/components/homemid.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect }  from "react";
 import photo from "./photo.png"
 import {TypeAnimation} from "react-type-animation";
 import "../App.css"
+import {createWebStorage} from "@emailjs/browser/es/utils/createWebStorage/createWebStorage";
 
 
 // ...
@@ -16,28 +17,35 @@ function Hoemid(){
         }, 0); // Adjust delay as needed
     }, []);
     return (
-        <div className="flex flex-col align-middle  font-urbanist font-bold">
 
-            <div className="border-2 ml-[45%] text-[30px] border-gray-700  h-14 pt-1 mt-9 w-[140px] pl-8 rounded-full">Hello</div>
-            <TypeAnimation className="ml-[35%] "
+        <div className="flex flex-col align-middle  font-urbanist font-bold mobile:h-[200vh] lg:h-[110vh] mobile:mt-[40vh] lg:mt-[0]">
+
+            <div className="border-2 lg:ml-[45%] lg:w-[165px]  mobile:ml-[75vw] lg:text-[30px] mobile:text-[90px] mobile:mb-[50px] mobile:h-[150px] mobile:w-[300px] mobile:pl-[50px]  border-gray-700  h-14 pt-0.5  mt-10 w-[140px] pl-8 rounded-full ">Hello</div>
+            <TypeAnimation className=" mobile:w-[200vw] lg:w-[100vw] mobile:ml-[25%] lg:ml-0"
                 preRenderFirstString={true}
                 sequence={['I\'m Omkar',1000,
                     'Full Stack devloper',1000,]}
                 speed={50}
-                style={{ fontSize: '70px'} }
+                           style={{
+                               fontSize: '70px',
+                               display: 'flex',
+                               justifyContent: 'center',
+                               alignItems: 'center',
+
+                           }}
                 repeat={Infinity}/>
 
-            <div className="flex justify-between w-[90%]">
-                <div className="ml-[7%]">
+            <div className="flex justify-between w-[90%] ">
+                <div className="ml-[7%] lg:inline mobile:hidden">
                     <p className="text-[70px] mt-28">**</p>
                     <div className="relative bottom-9 text-xl ">
                     <p>I Have been working on </p><p className="break-before-column">Full Stack devlopement </p><p
                     className="break-before-column">from past one Year</p>
                     </div>
                 </div>
-                <div className={`image ${loaded ? '' : 'hidden'}`}><img src={photo} className="lg:w-[800px] lg:h-[700px] mobile:w-[300px] mobile:h-[100px] relative bottom-[90px]" /></div>
-                <div>
-                <div className="text-pink-400 text-[100px] mt-28">*****</div>
+                <div className={`image ${loaded ? '' : 'hidden'}`}><img src={photo}  className=" lg:w-[900px] lg:h-700 mobile:ml-[55vw] mobile:w-[500px] lg:ml-0 relative lg:bottom-[120px] sm:bottom-0"  /></div>
+                <div className="lg:inline mobile:hidden ">
+                <div className="text-pink-400 text-[100px] mt-28 ">*****</div>
                   <div className="relative bottom-16">  <p className="text-4xl">
                         Developing
                     </p>
@@ -48,7 +56,7 @@ function Hoemid(){
                 </div>
 
             </div>
-           
+
 
         </div>
     )
--- src/components/page4.js
+++ src/components/page4.js
@@ -3,13 +3,13 @@ import Back from "./back.png"
 import {ContactUs} from "./mail";
 
 function Page4() {
-    return (<div class="pag4 bg-cover bg-center h-screen">
+    return (<div class="pag4 bg-cover bg-center h-screen  mobile:w-[290vw] lg:w-[100vw]">
         <div className="flex flex-col font-urbanist">
-       <div className="text-white font-urbanist text-5xl ml-[30%] pt-[5%] mb-[3%]">Have an awesome project idea? <p className="text-orange-500 mt-5 ml-[18%]">Let's Discuss</p></div>
+       <div className="text-white font-urbanist text-5xl lg:ml-[30%] lg:pt-[5%] lg:mb-[3%] mobile:w-[700px] ">Have an awesome project idea? <p className="text-orange-500 mt-5 ml-[18%]">Let's Discuss</p></div>
             <div className="text-white ml-[25%] w-[880px] text-xl   ">Have an awesome idea about a certain project ! have any doubt ? Collabrating with me !, any thing <p className="ml-[30%]">-- Contact me here, i will Do my Best</p>  </div>
 
         </div>
-        <div className="w-[700px] border-none rounded-3xl mt-[5%] h-[300px] flex justify-center align-middle ml-[30%] opacity-10 bg-white">
+        <div className="w-[700px] border-none rounded-3xl mt-[5%]  h-[300px] flex justify-center align-middle ml-[30%] opacity-10 bg-white">
             <p className="text-black text-2xl"></p>
             <ContactUs/>
         </div>
